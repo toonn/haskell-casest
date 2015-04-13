028763f3c77eaea409d9f1ae485cc9aa97fd1799
Toon Nolten <toon.nolten@student.kuleuven.be>
2015-04-13 21:21:42 +0200

    describe_table should be implemented in the agda code as a query because otherwise query return values have to be parsed in two places, in query and in connect

--- relational_base.hs
+++ relational_base.hs
@@ -4,21 +4,13 @@
     -- Toon Nolten
 
 -}
-module RelationalBase (connectSqlite3, describe_table, read) where
+module RelationalBase (connectSqlite3, read) where
 
 import Database.HDBC.sqlite3 (connectSqlite3)
 import Database.HDBC
 
 type DatabasePath = String
 type TableName = String
-
-describe_table :: IConnection conn => conn -> TableName
-                      -> IO [[SqlValue]]
-describe_table conn table =
-    quickQuery' conn
-        "SELECT name, type FROM sqlite_master "++
-        "WHERE tbl_name = "++ table ++" "++
-        "ORDER BY name"
 
 -- Read : all s -> Handle s -> RA s
 read :: IConnection conn => conn -> TableName
